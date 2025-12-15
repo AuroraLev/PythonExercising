3aba77f413b0a43894c96c17a97493a02c16ffb2
AuroraLev <auroradarco0@gmail.com>
2025-12-15 15:29:23 +0100

    adding ex.7, also ex.8 but I suck so I failed it ;-;

---
+++ Ex8.py
@@ -0,0 +1,13 @@
+#userNumb = int(input())
+
+#str_userNumb = str(userNumb)
+
+#i = 0
+
+#while i < len(str_userNumb):
+#    for i in range(0, len(str_userNumb)):    
+#        if str_userNumb[len(str_userNumb)-i-1] == str_userNumb[i]:
+#            print(f"{str_userNumb} is a palindrome number!")
+#        else:
+#            print(f"{str_userNumb} is NOT a palindrome number!")
+            
